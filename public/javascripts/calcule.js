35e711e011d9d1333daf9f1d4c9a4886691ac3c9
Adeline <adeline.servan@gmail.com>
2020-10-28 10:10:43 +0400

    modification affichage historique ajout gestion d'erreur

--- public/javascripts/calcule.js
+++ public/javascripts/calcule.js
@@ -4,7 +4,6 @@ let operation = document.getElementById('operation');
 let local = document.getElementById('local'); 
 let array = [];
 
-
  for (let i = 0; i < btn.length; i++) {
 
  	//ecoute le clic
@@ -31,41 +30,98 @@ let array = [];
  }
 
 function calcule (arg) {
+	
+	
+	//gestion cas user rentre que des opérateurs
+	if(isNaN(parseInt(arg) ) == true ){
+		
+		// affiche un message d'erreur
+		affiche.innerHTML = 'ERROR';
+		operation.innerHTML = 'ERROR';
+
+		return false;
+	}
 
 	//permet de calculer
 	var result = eval(arg);
-	var li = document.createElement('li');
-	li.classList.add('list-group-item', 'list-group-item-dark');
+
+	switch(result){
+		case undefined :
+			result = '0';
+			
+			//créer li + css 
+			var li = document.createElement('li');
+			li.classList.add('list-group-item', 'list-group-item-dark');
 
 
-	// stocke le réultat et le calcule
-	let storage = localStorage.setItem('historique', arg+'='+result);
-	let his = localStorage.getItem('historique' );
-	
-	// affiche le résultat et historique 
-	affiche.innerHTML = result;
-	operation.innerHTML = result;
-	
+			// stocke le réultat et le calcule
+			let storage = localStorage.setItem('historique',result+'='+result);
+			let his = localStorage.getItem('historique' );
 
-	// limite 
-	if(array.length == 10 ){
-	// 	//supprime le premier élément
-		array.shift();
-		array.push(arg+'='+result);
-		local.innerHTML = "";
+			// affiche le résultat et historique 
+			affiche.innerHTML = result;
+			operation.innerHTML = result;
 
-	// 	affiche les 10 dernières opérations
-	for (var j = 0; j < array.length; j++) {
-		local.append(li)
-		li.append(array[j])
+			// limite 
+			if(array.length == 10 ){
+			// 	//supprime le premier élément
+				array.shift();
+				array.push(result+'='+result);
+				local.innerHTML = "";
+
+			// 	affiche les 10 dernières opérations
+			for (var j = 0; j < array.length; j++) {
+				local.append(li)
+				li.append(array[j])
+			}
+
+			}else{
+			// 	ajout les éléments dans le tableau
+				array.push(result+'='+result);
+				local.append(li);
+				li.append(his);
+			}
+
+
+
+			break;
+
+			default :
+				var li = document.createElement('li');
+				li.classList.add('list-group-item', 'list-group-item-dark');
+
+
+				// stocke le réultat et le calcule
+				let storages = localStorage.setItem('historique', arg+'='+result);
+				let hist = localStorage.getItem('historique' );
+				
+				// affiche le résultat et historique 
+				affiche.innerHTML = result;
+				operation.innerHTML = result;
+				
+
+				// limite 
+				if(array.length == 10 ){
+				// 	//supprime le premier élément
+					array.shift();
+					array.push(arg+'='+result);
+					local.innerHTML = "";
+
+				// 	affiche les 10 dernières opérations
+				for (var j = 0; j < array.length; j++) {
+					local.append(li)
+					li.append(array[j])
+				}
+
+				}else{
+				// 	ajout les éléments dans le tableau
+					array.push(arg+'='+result);
+					local.append(li);
+					li.append(hist);
+				}
+			break;
 	}
 
-	}else{
-	// 	ajout les éléments dans le tableau
-		array.push(arg+'='+result);
-		local.append(li);
-		li.append(his);
-	}
 	
 }
 
